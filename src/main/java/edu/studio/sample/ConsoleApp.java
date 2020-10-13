b068195b52c82dcb36b97c3f0b8f12b8a62280eb
ms-O <kristin.obermyer@villanova.edu>
2020-10-13 09:11:42 -0400

    Change artifact id and update output message

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -4,7 +4,7 @@ public class ConsoleApp {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Maven");
         
     }
 
