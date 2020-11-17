3ada992e22353d382554d120acef19166955c6b3
liuleqi <lliu4@villanova.edu>
2020-11-18 00:53:49 +0800

    change output

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -4,7 +4,7 @@ public class ConsoleApp {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Maven");
+        System.out.println("Hello Maven HW5");
         
     }
 
